8efbf861e733c9b483123376336998412cf3bcab
Renata Brasil <renatabbss@gmail.com>
2021-11-22 21:12:25 -0300

    Ajuste na configuracao do banco de dados

--- mining_my_repository/settings/test.py
+++ mining_my_repository/settings/test.py
@@ -16,7 +16,7 @@ print(TESTS_IN_PROGRESS)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'TEST': {
             'NAME': 'test_database',
         },
