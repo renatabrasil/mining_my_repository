29576c09c72d3dec1e61ad44937c5d0891bab556
Renata Brasil <renatabbss@gmail.com>
2021-11-27 16:21:33 -0300

    Atualiza o git actions

--- mining_my_repository/settings/test.py
+++ mining_my_repository/settings/test.py
@@ -31,7 +31,7 @@ DATABASES['default'] = {
 # db_from_env = dj_database_url.config(default=config('DATABASE_URL'))
 # DATABASES['default'].update(db_from_env)
 
-# INSTALLED_APPS += ['contributions.tests.test_models', ]
+INSTALLED_APPS += ['contributions', ]
 
 LOGGING = {
     'version': 1,
