3dcb65d235557bdff894e55588e9e21b4f470f26
Renata Brasil <renatabbss@gmail.com>
2021-12-17 23:02:49 -0300

    Simplifica view

--- contributions/urls.py
+++ contributions/urls.py
@@ -2,10 +2,9 @@
 from django.urls import path
 
 # local Django
-from . import views2, views
+from . import views
 
 urlpatterns = [
-    path('2', views2.index, name='index'),
     path('', views.ContributionsListView.as_view(), name='index'),
     path('<int:pk>/', views.CommitDetailView.as_view(), name='detail'),
     path('commits/', views.CommitDetailView.as_view(), name='detail_by_hash'),
