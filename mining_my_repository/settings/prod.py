b48f0f8adcb46b89ecf06ee77b92335fcb1a7a11
Renata Brasil <renatabbss@gmail.com>
2021-12-25 18:53:13 -0300

    Adicionando sentry para observability

--- mining_my_repository/settings/prod.py
+++ mining_my_repository/settings/prod.py
@@ -1,1 +1,21 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 DEBUG = False
+
+# Observability
+sentry_sdk.init(
+    dsn="https://76f8b2c1a0f54883bcd435787eb68e5a@o1099084.ingest.sentry.io/6123558",
+    integrations=[DjangoIntegration()],
+    debug=True,
+    environment="production",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
