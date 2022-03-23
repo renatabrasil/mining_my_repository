1423b7333f7f490c6f0cbd543e71f7419800f450
Renata Brasil <renatabbss@gmail.com>
2022-03-23 18:48:45 -0300

    Teste de helper

--- mining_my_repository/wsgi.py
+++ mining_my_repository/wsgi.py
@@ -1,15 +1,8 @@
-"""
-WSGI config for mining_my_repository project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
 import os
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mining_my_repository.settings.dev')
-
-application = get_wsgi_application()
+def application(environ, start_response):
+    if environ['mod_wsgi.process_group'] != '':
+        import signal
+        os.kill(os.getpid(), signal.SIGINT)
+    return ["killed"]
