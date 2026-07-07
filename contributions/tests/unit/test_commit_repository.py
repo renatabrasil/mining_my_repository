fc168c66838de6dea0bb6fbce5404b786ece56cb
Renata Brasil <rebra@example.com>
2026-07-07 19:49:20 -0300

    Add contributions unit test coverage

--- contributions/tests/unit/test_commit_repository.py
+++ contributions/tests/unit/test_commit_repository.py
@@ -78,3 +78,58 @@ class DeveloperModelTests(TransactionTestCase):
 
         # Then
         self.assertEqual(expected_result, list(result))
+
+    def test_should_find_all_distinct_commits_by_tag_description(self):
+        project = Project.objects.create(project_name="ANT")
+        tag = Tag.objects.create(description="v1.0", project=project)
+        other_tag = Tag.objects.create(description="v2.0", project=project)
+        expected_commit = Commit.objects.create(hash="MATCH", author=Developer(name="Franco"),
+                                                committer=Developer(name="Roberto"), tag=tag)
+        Commit.objects.create(hash="OTHER", author=Developer(name="Fred"),
+                              committer=Developer(name="Roberto"), tag=other_tag)
+
+        result = self.repository.find_all_distinct_commits_by_tag_description("v1.0")
+
+        self.assertListEqual([expected_commit], list(result))
+
+    def test_should_find_all_commits_by_author_id(self):
+        project = Project.objects.create(project_name="ANT")
+        tag = Tag.objects.create(description="v1.0", project=project)
+        author = Developer.objects.create(name="Franco")
+        other_author = Developer.objects.create(name="Fred")
+        expected_commit = Commit.objects.create(hash="AUTHOR", author=author,
+                                                committer=Developer(name="Roberto"), tag=tag)
+        Commit.objects.create(hash="OTHER_AUTHOR", author=other_author,
+                              committer=Developer(name="Roberto"), tag=tag)
+
+        result = self.repository.find_all_commits_by_author_id(author.id)
+
+        self.assertListEqual([expected_commit], list(result))
+
+    def test_should_find_commits_by_project_ordered_by_id(self):
+        project = Project.objects.create(project_name="ANT")
+        other_project = Project.objects.create(project_name="Maven")
+        tag = Tag.objects.create(description="v1.0", project=project)
+        other_tag = Tag.objects.create(description="v1.0", project=other_project)
+        second_commit = Commit.objects.create(id=20, hash="SECOND", author=Developer(name="Franco"),
+                                              committer=Developer(name="Roberto"), tag=tag)
+        first_commit = Commit.objects.create(id=10, hash="FIRST", author=Developer(name="Fred"),
+                                             committer=Developer(name="Roberto"), tag=tag)
+        Commit.objects.create(id=30, hash="OTHER_PROJECT", author=Developer(name="Ana"),
+                              committer=Developer(name="Roberto"), tag=other_tag)
+
+        result = self.repository.find_all_commits_by_project_order_by_id_asc(project)
+
+        self.assertListEqual([first_commit, second_commit], list(result))
+
+    def test_should_find_only_compilable_commits_by_hash(self):
+        project = Project.objects.create(project_name="ANT")
+        tag = Tag.objects.create(description="v1.0", project=project)
+        expected_commit = Commit.objects.create(hash="SAME_HASH", compilable=True, author=Developer(name="Franco"),
+                                                committer=Developer(name="Roberto"), tag=tag)
+        Commit.objects.create(hash="SAME_HASH", compilable=False, author=Developer(name="Fred"),
+                              committer=Developer(name="Roberto"), tag=tag)
+
+        result = self.repository.find_all_compilable_commits_by_hash("SAME_HASH")
+
+        self.assertListEqual([expected_commit], list(result))
--- contributions/tests/unit/test_context_processor.py
+++ contributions/tests/unit/test_context_processor.py
@@ -1,6 +1,36 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
+
+from contributions.context_processor import tags_processor
+from contributions.models import Developer, Project, Tag
 
 
 class ContextProcessorTests(TestCase):
-    def test_tags_processor(self):
-        pass  # TODO: implement
+    def setUp(self):
+        self.request_factory = RequestFactory()
+        self.project = Project.objects.create(project_name="ANT")
+        self.other_project = Project.objects.create(project_name="Maven")
+        self.major_tag = Tag.objects.create(description="v1.0", project=self.project, major=True)
+        Tag.objects.create(description="v1.1-minor", project=self.project, major=False)
+        Tag.objects.create(description="v2.0", project=self.other_project, major=True)
+        self.developer = Developer.objects.create(name="Ana", email="ana@example.com", login="ana")
+
+    def test_tags_processor_uses_project_from_session(self):
+        request = self.request_factory.get("/")
+        request.session = {"project": self.project.id}
+
+        context = tags_processor(request)
+
+        self.assertEqual(self.project, context["project"])
+        self.assertListEqual([self.major_tag], list(context["tags"]))
+        self.assertListEqual([self.developer], list(context["developers"]))
+        self.assertCountEqual([self.project, self.other_project], list(context["projects"]))
+
+    def test_tags_processor_falls_back_to_first_project_when_session_project_is_empty(self):
+        request = self.request_factory.get("/")
+        request.session = {"project": None}
+
+        context = tags_processor(request)
+
+        self.assertEqual(self.project, context["project"])
+        self.assertListEqual([self.major_tag], list(context["tags"]))
